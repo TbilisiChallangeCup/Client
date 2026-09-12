16544784a8196da4a4cfd34716e5dc5ff181d2dc
justzeestuff <justsomezeestuff@gmail.com>
2026-09-12 17:24:44 +0400

    calendar & news json fetch methods

--- app/page/calendar/page.tsx
+++ app/page/calendar/page.tsx
@@ -1,28 +1,64 @@
+'use client'
+import { useState, useEffect } from 'react'
 import Dropdown from '@/app/assets/components/dropdown/dropdown'
 import style from './style.module.css'
 
-interface YearData {
-  [year: number]:
-  {
-    tour: number,   
-    team1Points: number,
-    team2Points: number,
-  }[]
+type Match = {
+  tour: number,
+  team1: string,
+  team1Points: number,
+  team2: string,
+  team2Points: number,
+}
+type Structure = {
+  [year: string]: Match[]
 }
 
 const Calendar = () => {
+  const [years, setYears] = useState<string[]>([])
+  const [data, setData] = useState<Structure>({})
+  const [selected, setSelected] = useState<string>('')
+
+  async function FetchData() {
+    try {
+      const res = await fetch('https://raw.githubusercontent.com/TbilisiChallangeCup/database/main/Calendar.json')
+      const json = JSON.parse(await res.text());
+      return json
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res: Structure = await FetchData();
+        const yearsArr = Object.keys(res).map(key => key)
+
+        setYears(yearsArr)
+        setData(res) 
+        setSelected(yearsArr[0])
+      } 
+      catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <>
-      <main className={style.main} >
+      <main className={style.main}>
         <section className={`${style.header} default`}>
-          <h1>კალენდარი </h1>
+          <h1>კალენდარი</h1>
           <p>ტურნირის ოფიციალური მატჩები მიმდინარე ეტაპისთვის.</p>
         </section>
+
         <div className={`${style.calendar} default`}>
           <section className={style.title}>
-            <div className={style.date} >
+            <div className={style.date}>
               <p>ტური</p>
-              <Dropdown className={style.dropdown} contents={['2012']} currentSelection={} />
+              <Dropdown className={style.dropdown} contents={years} currentSelection={setSelected} />
             </div>
             <div>
               <p>გუნდი</p>
@@ -30,6 +66,19 @@ const Calendar = () => {
               <p>გუნდი</p>
             </div>
           </section>
+
+          {data[selected]?.map((match, i) => (
+            <section key={i} className={`${style.title} ${style.match}`}>
+              <div className={style.date}>
+                <p>{match.tour}</p>
+              </div>
+              <div>
+                <p>{match.team1}</p>
+                <p>{match.team1Points} - {match.team2Points}</p>
+                <p>{match.team2}</p>
+              </div>
+            </section>
+          ))}
         </div>
       </main>
     </>
--- app/page/news/page.tsx
+++ app/page/news/page.tsx
@@ -1,40 +1,54 @@
+'use client'
+import { useState, useEffect } from 'react'
 import style from './style.module.css'
 
+type NewsItem = {
+  date: string,
+  title: string,
+  description: string
+}
+
 const Table = () => {
-  const news = [
-    {
-      date: 'ტურნირი • აპრილი 2026',
-      title: 'გახსნის კვირის განრიგი დადასტურდა',
-      description: 'ტურნირი თბილისში ორი ასაკობრივი კატეგორიით დაიწყება და პირველივე დღეებში რამდენიმე მნიშვნელოვანი მატჩი გაიმართება.'
-    },
-        {
-      date: 'ტურნირი • აპრილი 2026',
-      title: 'გახსნის კვირის განრიგი დადასტურდა',
-      description: 'ტურნირი თბილისში ორი ასაკობრივი კატეგორიით დაიწყება და პირველივე დღეებში რამდენიმე მნიშვნელოვანი მატჩი გაიმართება.'
-    },
-        {
-      date: 'ტურნირი • აპრილი 2026',
-      title: 'გახსნის კვირის განრიგი დადასტურდა',
-      description: 'ტურნირი თბილისში ორი ასაკობრივი კატეგორიით დაიწყება და პირველივე დღეებში რამდენიმე მნიშვნელოვანი მატჩი გაიმართება.'
+  const [data, setData] = useState<NewsItem[]>([])
+
+  async function FetchData() {
+    try {
+      const res = await fetch('https://raw.githubusercontent.com/TbilisiChallangeCup/database/main/News.json')
+      const json = JSON.parse(await res.text())
+      return json
     }
-  ]
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const json = await FetchData();
+        setData(json)
+      }
+      catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <main>
         <section className={`${style.header} default`}>
-          <h1>სიახლეები </h1>
+          <h1>სიახლეები</h1>
           <p>ოფიციალური განცხადებები და ტურნირის განახლებები.</p>
         </section>
         <div className={`${style.news} default`}>
-          {
-            news.map((data, i)=>(
-              <div className='default' >
-                <p>{data.date}</p>
-                <p>{data.title}</p>
-                <p>{data.description}</p>
-              </div>
-            ))
-          }
+          {data.map((item, i) => (
+            <div className='default' key={i}>
+              {/* <p>{item.date}</p> */}
+              <p>{item.title}</p>
+              <p>{item.description}</p>
+            </div>
+          ))}
         </div>
       </main>
     </>
